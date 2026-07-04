464b4a9c2f5e7701d6fbbdc618b6f65b3abed935
Raju1173 <rajdeepsrivastava2009@gmail.com>
2026-07-04 22:18:50 +0530

    altered renaming logic to fix small ASM generation bug

--- test.c
+++ test.c
@@ -1,14 +1,14 @@
-int fact(int n)
+int fact(int n, int o, int p, int q, int r, int s, int t, int u)
 {
     if (n <= 1)
     {
         return 1;
     }
 
-    return n * fact(n - 1);
+    return n * fact(n - 1, 1, 2, 3, 4, 5, 6, 7);
 }
 
 int main()
 {
-    return fact(5);
+    return fact(5, 2, 3, 4, 4, 5, 6, 7);
 }
--- test.s
+++ test.s
@@ -5,8 +5,17 @@ fact:
 .L1:
 	push rbp
 	mov rbp, rsp
-	sub rsp, 24
-	mov DWORD PTR [rbp-24], edi
+	sub rsp, 48
+	mov DWORD PTR [rbp-8], edi
+	mov DWORD PTR [rbp-24], esi
+	mov DWORD PTR [rbp-28], edx
+	mov DWORD PTR [rbp-32], ecx
+	mov DWORD PTR [rbp-36], r8d
+	mov DWORD PTR [rbp-40], r9d
+	mov eax, DWORD PTR [rbp+16]
+	mov DWORD PTR [rbp-44], eax
+	mov eax, DWORD PTR [rbp+24]
+	mov DWORD PTR [rbp-48], eax
 	cmp DWORD PTR [rbp-8], 1
 	jle .L3
 	jmp .L2
@@ -19,13 +28,22 @@ fact:
 	jmp .L2
 
 .L2:
+	mov eax, 2
+	mov DWORD PTR [rbp-8], eax
 	mov eax, DWORD PTR [rbp-8]
 	sub eax, 1
 	mov DWORD PTR [rbp-12], eax
 	mov edi, DWORD PTR [rbp-12]
+	mov esi, 1
+	mov edx, 2
+	mov ecx, 3
+	mov r8d, 4
+	mov r9d, 5
+	push 7
+	push 6
 	call fact
 	mov DWORD PTR [rbp-16], eax
-	add rsp, 0
+	add rsp, 8
 	mov eax, DWORD PTR [rbp-8]
 	imul eax, DWORD PTR [rbp-16]
 	mov DWORD PTR [rbp-20], eax
@@ -42,9 +60,16 @@ main:
 	mov rbp, rsp
 	sub rsp, 8
 	mov edi, 5
+	mov esi, 2
+	mov edx, 3
+	mov ecx, 4
+	mov r8d, 4
+	mov r9d, 5
+	push 7
+	push 6
 	call fact
 	mov DWORD PTR [rbp-8], eax
-	add rsp, 0
+	add rsp, 8
 	mov eax, DWORD PTR [rbp-8]
 	mov rsp, rbp
 	pop rbp
